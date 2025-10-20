61c14586947bbda6562ef132f49fe122c892af24
JJavierBS <josejavierbravosarabia@gmail.com>
2025-10-20 22:27:42 +0200

    CORS modification

--- ZipRedir_backend/src/main/java/ZipRedir/ZipRedir/controller/ElementalController.java
+++ ZipRedir_backend/src/main/java/ZipRedir/ZipRedir/controller/ElementalController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.net.URI;
 import java.util.Map;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 @RestController
 public class ElementalController {
 
